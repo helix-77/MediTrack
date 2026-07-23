8a239ecbfa5d65c97c03bb9b2e1e7228852c8d38
helix-77 <mdatikmouhtasimrahi@gmail.com>
2026-07-23 17:57:40 +0600

    fix(android): resolve JVM target compatibility for flutter_timezone and library plugins

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,4 +51,3 @@ flutter {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
-
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,8 +15,25 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    plugins.withId("com.android.library") {
+        configure<com.android.build.gradle.LibraryExtension> {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
