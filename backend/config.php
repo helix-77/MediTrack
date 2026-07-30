f63974aed579c6ee8913f4124305fa2f130b1e46
helix-77 <mdatikmouhtasimrahi@gmail.com>
2026-07-30 15:26:53 +0600

    fix(backend): update BDAPPS_APP_PASSWORD credential in config.php

--- backend/config.php
+++ backend/config.php
@@ -12,4 +12,4 @@
  * developer.bdapps.com.
  */
 define('BDAPPS_APP_ID', 'APP_139363');
-define('BDAPPS_APP_PASSWORD', 'y0e74fafba35bd80a3e484ca07ab43715');
+define('BDAPPS_APP_PASSWORD', '0e74fafba35bd80a3e484ca07ab43715');
